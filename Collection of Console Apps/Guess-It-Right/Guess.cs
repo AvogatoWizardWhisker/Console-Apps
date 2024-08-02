699e2bb9499e64c282d0f99018e9ab62c7aeedcb
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-08-02 14:50:14 +0100

    Added functional code for the guessing game

--- Collection of Console Apps/Guess-It-Right/Guess.cs
+++ Collection of Console Apps/Guess-It-Right/Guess.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Collection_of_Console_Apps.Guess_It_Right
+Random random = new Random();
+int guess = 0;
+string welcomeMessage = "Welcome to Guess It Right. Guess a number between 1 and 500";
+int num = random.Next(1, 100);
+
+Console.WriteLine(welcomeMessage);
+Console.WriteLine("-----------------------------------------------------------");
+
+int i = 0;
+
+while (guess != num)
 {
-    internal class Guess
+    try
     {
+        guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess > num)
+        {
+            Console.WriteLine("Hmmmmm, too high. Guess again");
+        }
+
+        else
+        {
+            Console.WriteLine("Too low, try again");
+        }
     }
+    catch
+    {
+        Console.WriteLine("Guess must be a number");
+        i--;
+    }
+    i++;
 }
+Console.WriteLine($"Congrats, it took you {i} attempts");
+Console.ReadLine();
