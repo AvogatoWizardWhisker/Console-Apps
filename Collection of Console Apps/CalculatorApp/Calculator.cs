e63bf6448fbe74260c14511c264b4987cb2ae513
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-08-02 10:55:56 +0100

    Added a class for Calculator

---
+++ Collection of Console Apps/CalculatorApp/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_of_Console_Apps.CalculatorApp
+{
+    internal class Calculator
+    {
+
+    }
+}
