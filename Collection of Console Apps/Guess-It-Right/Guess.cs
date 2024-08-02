c4294d035ff54ffc63a42d361d5b5af83a27a1f1
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-08-02 10:55:33 +0100

    Added a class for guessing game

---
+++ Collection of Console Apps/Guess-It-Right/Guess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_of_Console_Apps.Guess_It_Right
+{
+    internal class Guess
+    {
+    }
+}
